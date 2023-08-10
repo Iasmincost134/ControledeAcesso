125ae070e242d571cff8fffd2da1326f8528347b
Iasmincost134 <133365214+Iasmincost134@users.noreply.github.com>
2023-08-10 11:24:29 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 function logar(){
     var numero = document.getElementById('numero').value;
     // let fundo = document.querySelector('#corpo')
-
+    // document.getElementById('trocar').style.backgroundColor = "blue"
+    var trocar = document.body;
     if(numero >= 1000 && numero <=1999){
         alert('Acesso total a todas as áreas restritas.');
-        // fundo.style.backgroundColor = '#ffbe0b'
+        trocar.style.backgroundColor = 'green';
+
     }
     else if (numero >= 2000 && numero <=2999){
         alert('acesso limitado a algumas áreas restritas.');
+        trocar.style.backgroundColor = 'yellow';
     }
     else{
-        alert('não têm  acesso para áreas restritas.')
+        trocar.style.backgroundColor = 'red';
+        alert('não têm acesso para áreas restritas.')
     }
+
+    // switch (logar) {
+    //     case "Acesso total a todas as áreas restritas.":
+    //         cor.backgroundColor = 'green';
+    //         break;
+    // }
 
 }
